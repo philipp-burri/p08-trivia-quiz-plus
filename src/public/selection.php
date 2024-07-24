22135b8f4c399b857ca1d5cc2925881777593df1
philipp-burri <burri.philipp13@gmail.com>
2024-07-24 22:54:23 +0200

    changed landing page animation and font, item placement on selection page

--- src/public/selection.php
+++ src/public/selection.php
@@ -13,7 +13,7 @@
 <body>
   <?php include '../utils/header.php'; ?>
     <div class="selection-container">
-        <h1 class="title" id="selectionTitle">WÄHLE DEINE KATEGORIE</h1>
+        <h1 class="title" id="selectionTitle">KATEGORIE</h1>
         <div class="card-container" id="categories">
             <div class="card" data-category="football">
                 <div class="card-inner">
