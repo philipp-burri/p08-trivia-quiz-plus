bbd9af956286e5c040be31f65c91756f8db14ad8
philipp-burri <burri.philipp13@gmail.com>
2024-07-25 09:57:42 +0200

    merge new design into main

--- src/public/index.php
+++ src/public/index.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TriviaQuiz++</title>
-    <link rel="stylesheet" href="assets/css/index.css"/>
-    <script src="assets/js/main.js"></script>
-
-</head>
-
-<body>
 <!DOCTYPE html>
 <html lang="de">
 <head>
@@ -19,10 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TriviaQuiz++</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/index.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body>
-    <div class="container">
+
+    <div class="index-container">
         <h1 class="title">TRIVIAQUIZ</h1>
         <a href="selection.php" class="start-button">START QUIZ</a>
     </div>
@@ -40,7 +29,8 @@
                     <li></li>
                     <li></li>
             </ul>
-    </div >
+    </div>
+   
 </body>
 </html>
 </body>
