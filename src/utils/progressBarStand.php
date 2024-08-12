478fb30dc5c767c282e592bed0b0cde0641b4969
philipp-burri <burri.philipp13@gmail.com>
2024-08-12 14:27:31 +0200

    merge with dani

--- src/utils/progressBarStand.php
+++ src/utils/progressBarStand.php
@@ -2,25 +2,25 @@
 echo 
     '<div class="progress-container fadeInElement">' .
         '<div class="progress-bar">' .
-            '<div class="step active"></div>' .
+            '<div class="step active">1</div>' .
             '<div class="bar active"></div>' .
-            '<div class="step"></div>' .
+            '<div class="step">2</div>' .
             '<div class="bar"></div>' .
-            '<div class="step"></div>' .
+            '<div class="step">3</div>' .
             '<div class="bar"></div>' .
-            '<div class="step"></div>' .
+            '<div class="step">4</div>' .
             '<div class="bar"></div>' .
-            '<div class="step"></div>' .
+            '<div class="step">5</div>' .
             '<div class="bar"></div>' .
-            '<div class="step"></div>' .
+            '<div class="step">6</div>' .
             '<div class="bar"></div>' .
-            '<div class="step"></div>' .
+            '<div class="step">7</div>' .
             '<div class="bar"></div>' .
-            '<div class="step"></div>' .
+            '<div class="step">8</div>' .
             '<div class="bar"></div>' .
-            '<div class="step"></div>' .
+            '<div class="step">9</div>' .
             '<div class="bar"></div>' .
-            '<div class="step"></div>' .
+            '<div class="step">10</div>' .
         '</div>' .
     '</div>';
 
